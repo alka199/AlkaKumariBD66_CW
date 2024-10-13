94ab44ad8b80c64d5a116e9f3201aac2445c37f9
Alka Kumari <110741315+alka199@users.noreply.github.com>
2024-10-13 11:33:45 +0000

    Initial commit

---
+++ BD6.6_CW/index.js
@@ -0,0 +1,24 @@
+const cors = require("cors");
+const express = require("express");
+const { getAllEmployees, getEmployeesById } = require("./controllers");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//Exercise 1: Retrieve All Employees
+app.get("/employees", async (req, res) => {
+  const employees = getAllEmployees();
+  res.json({ employees });
+});
+
+//Exercise 2: Retrieve Employee by ID
+app.get("/employees/details/:id", async (req, res) => {
+  let employee = getEmployeesById(req.params.id);
+  res.json({
+    employee,
+  });
+});
+
+module.exports = { app };
